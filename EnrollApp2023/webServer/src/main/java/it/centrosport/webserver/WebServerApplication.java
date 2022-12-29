4cae8b2c5040c9cc293ebad7ae093ddca0348869
AandreaA-commits <a.arici4@studenti.unibg.it>
2022-12-29 14:23:58 +0100

    ridenominazione package webserver

--- EnrollApp2023/webServer/src/main/java/it/centrosport/webserver/WebServerApplication.java
+++ EnrollApp2023/webServer/src/main/java/it/centrosport/webserver/WebServerApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package it.centrosport.webserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
