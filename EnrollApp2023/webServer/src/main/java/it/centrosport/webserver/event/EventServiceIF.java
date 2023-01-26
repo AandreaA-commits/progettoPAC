7d4eb83a431df9f8399e09599a36969bcf61d731
AandreaA-commits <a.arici4@studenti.unibg.it>
2023-01-26 11:34:43 +0100

    Added User Interfaces
    Aggiunta delle interfacce per la gestione degli User

--- EnrollApp2023/webServer/src/main/java/it/centrosport/webserver/event/EventServiceIF.java
+++ EnrollApp2023/webServer/src/main/java/it/centrosport/webserver/event/EventServiceIF.java
@@ -3,12 +3,12 @@ package it.centrosport.webserver.event;
 import java.util.List;
 
 public interface EventServiceIF {
-	
+
 	public Event getEvent(String id);
-	
+
 	public List<Event> getEvents();
-	
+
 	public Event createEvent(Event event);
-	
+
 	public void deleteEvent(String id);
 }
---
+++ EnrollApp2023/webServer/src/main/java/it/centrosport/webserver/user/UserServiceIF.java
@@ -0,0 +1,18 @@
+package it.centrosport.webserver.user;
+
+import java.util.List;
+
+public interface UserServiceIF {
+	public List<User> getUsers();
+
+	public User getUser(String id);
+
+	public User signUp(User newUser);
+
+	public User login(User loggingUser);
+
+	public void logout(User loggingOutUser);
+
+	public void deleteUser(String id);
+
+}
