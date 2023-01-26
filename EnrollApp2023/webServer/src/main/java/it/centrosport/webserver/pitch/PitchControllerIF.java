09e5177bf2eea51a47118c5cf1661ca61bc74c63
AandreaA-commits <a.arici4@studenti.unibg.it>
2023-01-26 14:23:30 +0100

    Documentazione: aggiornamento diagrammi
    Consistenza tra documentazione e codice IT1 raggiunta

--- EnrollApp2023/webServer/src/main/java/it/centrosport/webserver/pitch/PitchControllerIF.java
+++ EnrollApp2023/webServer/src/main/java/it/centrosport/webserver/pitch/PitchControllerIF.java
@@ -5,7 +5,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 public interface PitchControllerIF {
 	public Iterable<Pitch> getPitches();
+
 	public Pitch getPitch(@PathVariable String pitchId);
+
 	public void deletePitch(@PathVariable("pitchId") String pitchId);
+
 	public Pitch createPitch(@RequestBody PitchDto pitchDto);
 }
--- EnrollApp2023/webServer/src/main/java/it/centrosport/webserver/pitch/PitchServiceIF.java
+++ EnrollApp2023/webServer/src/main/java/it/centrosport/webserver/pitch/PitchServiceIF.java
@@ -4,7 +4,10 @@ import java.util.List;
 
 public interface PitchServiceIF {
 	public Pitch getPitch(String id);
+
 	public List<Pitch> getPitches();
+
 	public Pitch createPitch(Pitch pitch);
+
 	public void deletePitch(String id);
 }
