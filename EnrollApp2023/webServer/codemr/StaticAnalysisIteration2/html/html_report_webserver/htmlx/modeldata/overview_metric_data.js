26eec0b9e09694de4f78722f0cb85da21a9f2318
Lorenzo Fagiani <l.fagiani1@studenti.unibg.it>
2023-03-01 15:25:49 +0100

    CodeMR analysis

---
+++ EnrollApp2023/webServer/codemr/StaticAnalysisIteration2/html/html_report_webserver/htmlx/modeldata/overview_metric_data.js
@@ -0,0 +1,1 @@
+var CodeMR_ProjectMetrics = {"Project Name":"webServer working set: {webServer}", "Number of Highly Problematic Classes":0, "Number of Entities":33, "Number of Problematic Classes":0, "Class Lines of Code":699, "Number of External Packages":9, "Number of Packages":5, "Number of External Entities":18}
